e6832fa522173ce09b492eadd1a2ab925d764628
Việt Nguyễn <vietnguyen2x@gmail.com>
2021-02-03 18:38:57 +0700

    change index.js add dir public,resources,scss,...

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars  = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'resources/views'));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
